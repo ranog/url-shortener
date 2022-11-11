2126824be953b01c0d00c0594de37228c14accfd
ranog <jprnogueira@yahoo.com.br>
2022-11-10 22:05:11 -0300

    add endpoints

--- src/main.py
+++ src/main.py
@@ -7,3 +7,13 @@ app = FastAPI()
 @app.get('/v1/ping/')
 async def root():
     return {'ping': 'pong'}
+
+
+@app.post('/v1/data/shorten/')
+async def shorten(json: dict):
+    pass
+
+
+@app.get('/v1/{short_url}/', status_code=301)
+async def short(short_url: str):
+    pass
--- tests/e2e/test_main.py
+++ tests/e2e/test_main.py
@@ -8,3 +8,15 @@ async def test_it_should_ping_successfully():
         response = await aysnc_client.get('/v1/ping/')
     assert response.status_code == 200
     assert response.json() == {'ping': 'pong'}
+
+
+async def test_it_should_shorten_successfully():
+    async with AsyncClient(app=app, base_url='http://test') as aysnc_client:
+        response = await aysnc_client.post('/v1/data/shorten/', json={})
+    assert response.status_code == 200
+
+
+async def test_it_should_short_url_successfully():
+    async with AsyncClient(app=app, base_url='http://test') as aysnc_client:
+        response = await aysnc_client.get('/v1/short_url/')
+    assert response.status_code == 301
